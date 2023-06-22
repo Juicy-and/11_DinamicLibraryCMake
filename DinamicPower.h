b69d04c121c82ca681111faabc809f17b51c8850
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2023-06-23 01:02:31 +0300

    Update DinamicPower.h

--- DinamicPower.h
+++ DinamicPower.h
@@ -1,18 +1,11 @@
 #pragma once
 #include <iostream>
 
-#ifdef DINAMICLIBRARY_EXPORTS
-#define DINAMICPOWER_API __declspec(dllexport)
-#else
-#define DINAMICPOWER_API __declspec(dllimport)
-#endif
-
 namespace DinamicPower
 {
 	class Leaver
 	{
 	public:
-		DINAMICPOWER_API
-		void leave(std::string name);
+	   void leave(std::string name);
 	};
 }
